ba289c90b0c76d2f39783b7c18fd64e9765ddf36
Bredell_Evans <bredy452@gmail.com>
2021-05-22 12:52:22 -0400

    got a different calendar working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './App.css';
-import {Inject, ScheduleComponent, Day, Month } from '@syncfusion/ej2-react-schedule'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import InteractionPlugin from '@fullcalendar/interaction'
+// import {Inject, ScheduleComponent, Day, Month } from '@syncfusion/ej2-react-schedule'
 
 
-export default class App extends React.Component{
+export default class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      title: 'Unavailable',
+      date: '2021-05-22'
+    }
+  }
   render() {
-    
+    // console.log(props)
     return (
-      <ScheduleComponent currentView='Month'>
-        <Inject services={[Day, Month]}/>
-      </ScheduleComponent>
+      <FullCalendar 
+      plugins={[dayGridPlugin, InteractionPlugin]} 
+      intialView='dayGridMonth' 
+      events={[
+        {title: this.state.title, date: this.state.date},
+        {title: this.state.title, date: '2021-05-20'}
 
-      )
+        ]}
+      dateClick={this.handleDateClick}/>
+
+    )
+
+
+    // return (
+    //   <ScheduleComponent currentView='Month'>
+    //     <Inject services={[Day, Month]}/>
+    //   </ScheduleComponent>
+
+    //   )
+  }
+
+  handleDateClick = (arg) => {
+    alert(this.title)
   }
 
 }
