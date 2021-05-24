c88d2a488ca1259983c7318cbb5508c3dc1fdb27
Bredell_Evans <bredy452@gmail.com>
2021-05-23 21:52:59 -0400

    undo button works and unavailable appears when clicked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react'
 import './App.css';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import InteractionPlugin from '@fullcalendar/interaction'
-// import {Inject, ScheduleComponent, Day, Month } from '@syncfusion/ej2-react-schedule'
+import InteractionPlugin from '@fullcalendar/interaction' 
+// import invert from 'lodash.invert'
 
 
 export default class App extends Component{
@@ -12,39 +12,69 @@ export default class App extends Component{
     this.state = {
       title: '',
       date: [],
-      info: []
+      availability: []
     }
   }
 
   handleDateClick = (e) => {
-    alert(e.dateStr)
-    const copyState = [...this.state.date]
-    copyState.push(e.dateStr)
-    this.setState({
-      date: copyState
+    
+    const dupState = {}
+    dupState.date = e.dateStr
+    const copyState = [...this.state.availability]
+    copyState.push(dupState)
+
+    copyState.forEach((data) => {
+      data.title = 'unavailable'
+      
+
     })
+
+    // copyState[0].title='unavailable'
+    // console.log(copyState[0])
+    this.setState({
+      availability: copyState
+    })
+
+    
+    console.log(this.state.availability)
+    console.log(e)
+  
+  }
+
+  undoEntry = (e) => {
+    const copyState = [...this.state.availability]
+    copyState.splice(-1)
+
+    this.setState({
+      availability: copyState
+    })
+
   }
 
   render() {
-    console.log(FullCalendar.events)
-    const events = [{title: 'unavailable', date: '2021-05-21'}]
+
+    const data = this.state.availability
+    
+    // info.title = 'unavailable'
+    // info.date = '2021-05-27'
+    // data.push(info)
+    // info = {}
+    // console.log(info)
+    // console.log(this.state.date)
+    
 
     return (
+      <>
       <FullCalendar 
       plugins={[dayGridPlugin, InteractionPlugin]} 
       intialView='dayGridMonth' 
-      events={events}
+      events={data}
       dateClick={(e)=> this.handleDateClick(e)}/>
 
+      <button onClick={(e) => {this.undoEntry(e)}}>undo</button>
+      </>
     )
 
-
-    // return (
-    //   <ScheduleComponent currentView='Month'>
-    //     <Inject services={[Day, Month]}/>
-    //   </ScheduleComponent>
-
-    //   )
   }
 
 }
