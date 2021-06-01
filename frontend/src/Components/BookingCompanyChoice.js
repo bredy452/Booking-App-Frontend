e27e7cd97249c4dc98585b1f13684b88815154c4
Bredell_Evans <bredy452@gmail.com>
2021-06-01 17:29:32 -0400

    got client view schedule working

--- frontend/src/Components/BookingCompanyChoice.js
+++ frontend/src/Components/BookingCompanyChoice.js
@@ -1,14 +1,62 @@
 import React, { Component} from 'react'
-import {Dropdown} from 'semantic-ui-react'
+import {Dropdown, Button} from 'semantic-ui-react'
 
 
 export default class BookingCompanyChoice extends Component {
 	constructor(props) {
 		super(props)
 		this.state={
-			companies: []
+			companies: [],
+			companyChoice: []
 		}
 	}
+
+	pushSchedule = (e) => {
+		e.preventDefault()
+		this.setState({
+			companyChoice: e.target.textContent
+		})
+	}
+
+	getSchedule = (e) => {
+		fetch(this.props.baseUrl + `/schedules/${this.state.companyChoice}`, {
+			credentials: 'include'})
+		.then(res => {
+			return res.json()
+		}).then(data => {
+
+			let arr = []
+    		arr = data.data.availability.split(',')
+    		let newArr = []
+    		let masterArr = []
+
+    		for(let j = 0; j < arr.length; j += 6){
+      			let bitArr = arr.slice(j, j + 6)
+      			newArr.push(bitArr)
+    		}
+
+    		for(let x = 0; x < newArr.length; x++){
+      			let chunk = newArr[x]
+      			let tempObj ={}
+
+      			for(let y = 0; y < chunk.length; y++) {
+          			if (y % 2 ==0){
+          				let key = chunk[y]
+          				let value = chunk[y+1]
+          				tempObj[key] = value
+      				}
+    			}
+
+    				masterArr.push(tempObj)
+  			}
+  			// console.log(masterArr)
+  			this.props.bookingSchedule(masterArr)
+  			// this.setState({
+  			// 	org_availability: masterArr
+  			// })
+		})
+	}
+
 
 	getCompanies = () => {
 		fetch(this.props.baseUrl + '/users/organizations', {
@@ -19,23 +67,15 @@ export default class BookingCompanyChoice extends Component {
 			let companies = []
 
 			data.organizations[0].forEach((name, index) => {
-				companies.push({'text': name.org_name, 'value': name.org_name})
-				
-				
+				companies.push({'text': name.org_name, 'value': name.org_name})		
 			})
 			
 			this.setState({
 				companies: companies
 			})
-			// companies.forEach(company => {
-			// 	company.title=company
-			// })
 			
 			console.log(this.state.companies)
 			console.log(companies)
-			// this.setState({
-			// 	companies: companiesList
-			// })
 		})
 
 	}
@@ -49,13 +89,19 @@ export default class BookingCompanyChoice extends Component {
 		let companyOptions = this.state.companies
 
 		return(
+			<>
 			<Dropdown
     			placeholder='Company Selection'
     			fluid
     			options={companyOptions}
     			search
     			selection
+    			onChange={(e) => {this.pushSchedule(e)}} 
+    			// value={this.state.companyChoice}
   			/>
+
+  			<Button schedule onClick={(e) => {this.getSchedule(e)}}>View Availability</Button>
+  			</>
 		)
 	}
 }
--- frontend/src/Components/ClientPage.js
+++ frontend/src/Components/ClientPage.js
@@ -13,44 +13,48 @@ export default class ClientPage extends Component {
 		}
 	}
 
-	getCalender = () => {
-		fetch(this.props.baseUrl + '/schedules/org_user/viewSchedule', {
-			credentials: 'include'})
-		.then(res => {
-			return res.json()
-		}).then(data => {
-			console.log(data.data[0].org_id.org_name)
-			let arr = []
-    		arr = data.data[0].availability.split(',')
-    		let newArr = []
-    		let masterArr = []
-
-//   //first loop slices the variable arr into chunck of 6 indexes and pushes to a new arr to make array of arrays (newArr)
-    		for(let j = 0; j < arr.length; j += 6){
-      			let bitArr = arr.slice(j, j + 6)
-      			newArr.push(bitArr)
-    		}
-
-// //next loop iterates throught each array in the newArr and isolates it into a chunk (which is one array at a time) and declares a blank obj for each chunk.
-    		for(let x = 0; x < newArr.length; x++){
-      			let chunk = newArr[x]
-      			let tempObj ={}
-//    //next loop iterates thrugh each index within the chunk. if it is even it becomes a key and the next index +1 becomes the value. then it adds the key and value to the tempObj
-      			for(let y = 0; y < chunk.length; y++) {
-          			if (y % 2 ==0){
-          				let key = chunk[y]
-          				let value = chunk[y+1]
-          				tempObj[key] = value
-      				}
-    			}
-//    //this is part of the x loop, once the the y loop goes through each index in the chunk, it pushes the tempObj to the masterArr
-    				masterArr.push(tempObj)
-  			}
-  			this.setState({
-  				org_availability: masterArr
-  			})
+	bookingSchedule = (scheduleInfo) => {
+		this.setState({
+			org_availability: scheduleInfo
 		})
-	}
+		console.log(this.state.org_availability)
+// 		fetch(this.props.baseUrl + '/schedules/org_user/viewSchedule', {
+// 			credentials: 'include'})
+// 		.then(res => {
+// 			return res.json()
+// 		}).then(data => {
+// 			console.log(data.data[0].org_id.org_name)
+// 			let arr = []
+//     		arr = data.data[0].availability.split(',')
+//     		let newArr = []
+//     		let masterArr = []
+
+// //   //first loop slices the variable arr into chunck of 6 indexes and pushes to a new arr to make array of arrays (newArr)
+//     		for(let j = 0; j < arr.length; j += 6){
+//       			let bitArr = arr.slice(j, j + 6)
+//       			newArr.push(bitArr)
+//     		}
+
+// // //next loop iterates throught each array in the newArr and isolates it into a chunk (which is one array at a time) and declares a blank obj for each chunk.
+//     		for(let x = 0; x < newArr.length; x++){
+//       			let chunk = newArr[x]
+//       			let tempObj ={}
+// //    //next loop iterates thrugh each index within the chunk. if it is even it becomes a key and the next index +1 becomes the value. then it adds the key and value to the tempObj
+//       			for(let y = 0; y < chunk.length; y++) {
+//           			if (y % 2 ==0){
+//           				let key = chunk[y]
+//           				let value = chunk[y+1]
+//           				tempObj[key] = value
+//       				}
+//     			}
+// //    //this is part of the x loop, once the the y loop goes through each index in the chunk, it pushes the tempObj to the masterArr
+//     				masterArr.push(tempObj)
+//   			}
+//   			this.setState({
+//   				org_availability: masterArr
+//   			})
+// 		})
+ 	}
 
 	// componentDidMount() {
 	// 	this.getCalender()
@@ -75,7 +79,7 @@ export default class ClientPage extends Component {
       		events={clientData}
       		selectable='true'/>*/}
 
-      		<BookingCompanyChoice baseUrl={this.props.baseUrl}/>
+      		<BookingCompanyChoice bookingSchedule={this.bookingSchedule} baseUrl={this.props.baseUrl}/>
       		</>
 
 		)
