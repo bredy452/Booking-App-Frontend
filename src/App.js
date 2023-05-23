9d2854d3b4d0fc0a9c6ce2bd2e5e77550d3cfdfd
Bredy Evans <Bredell Evans>
2023-05-23 03:59:43 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -15,20 +15,13 @@ import bootstrapPlugin from '@fullcalendar/bootstrap'
 import {Button} from 'semantic-ui-react'
 
 // import invert from 'lodash.invert'
-let baseUrl = ''
+let baseUrl = process.env.REACT_APP_BASEURL
 
 // if (process.env.NODE_ENV === 'development') {
 //   baseUrl= 'http://localhost:8000'
 // } else {
 //   baseUrl= process.env.REACT_APP_BASEURL
 // }
-if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'http://localhost:8000'
-}
-
-if (process.env.NODE_ENV === 'production') {
-  baseUrl = process.env.REACT_APP_BASEURL
-}
 
 export default class App extends Component{
   constructor(props){
